cd51f9996d8f48e18630cf9a2cb0b87c56142c4b
deven-ald <70286767+deven-ald@users.noreply.github.com>
2022-10-29 17:32:37 -0400

    Update MusicLibrary Tables.sql

--- MusicLibrary Tables.sql
+++ MusicLibrary Tables.sql
@@ -75,3 +75,60 @@ CREATE TABLE SONG_ALBUM(
 	constraint SONG_ALBUM_SongID_fk foreign key (SongID) references SONG(SongID),
 	constraint SONG_ALBUMAlbumID_fk foreign key (AlbumID) references ALBUM(AlbumID)
 )
+--The Following Code was created by Robert
+create table BAND (
+    BandID int identity(2000,1),
+    BandName char(20) not null,
+    MembersNum tinyint not null,
+    DateFormed date not null,
+    DateDisbanded date,
+    LabelID int not null,
+
+    constraint band_bandid_pk primary key(BandID),
+    constraint band_labelid_fk foreign key(LabelID) references LABEL(LabelID),
+    constraint band_membersnum_ck check (MembersNum > 0)
+);
+go 
+
+create table ARTIST (
+    ArtistID int identity(3000, 1),
+    ArtistName char(20) not null,
+    ArtistRegion char(20),
+    LabelID int not null,
+
+    constraint artist_artistid_pk primary key(ArtistID),
+    constraint artist_labelid_fk foreign key(LabelID) references LABEL(LabelID)
+);
+go
+
+create table BAND_ARTIST (
+    BandID int,
+    ArtistID int,
+    DateJoined date not null,
+    DateLeft date,
+    
+    constraint band_artist_bandartistid_pk primary key(BandID, ArtistID),
+    constraint band_artist_bandid_fk foreign key(BandID) references BAND(BandID),
+    constraint band_artist_artistid_fk foreign key(ArtistID) references ARTIST(ArtistID)
+);
+go
+
+create table SONG_BAND (
+    SongID int,
+    BandID int,
+
+    constraint song_band_songbandid_pk primary key(SongID, BandID),
+    -- constraint song_band_songid_fk foreign key(SongID) references SONG(SongID),
+    constraint song_band_bandid_fk foreign key(BandID) references BAND(BandID)
+);
+go
+
+create table SONG_ARTIST (
+    SongID int,
+    ArtistID int,
+
+    constraint song_artist_songartistid_pk primary key(SongID, ArtistID),
+    -- constraint song_artist_songid_fk foreign key(SongID) references SONG(SongID),
+    constraint song_artist_artistid_fk foreign key(ArtistID) references ARTIST(ArtistID)
+);
+go
